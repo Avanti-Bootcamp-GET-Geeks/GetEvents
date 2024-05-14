d8a428123e55117a1e1df25a32ac2cffce6d108b
Giovane Souza <developergiovanesouza@gmail.com>
2024-05-13 22:22:47 -0300

    refactor: add navigate + passagem de state

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,11 @@
-import styles from './card.module.css'
+import { useNavigate } from 'react-router-dom';
+import './Card.css'
 import { Calendar2Event, ChevronRight, PinMap } from 'react-bootstrap-icons';
 
 /* Event recebe TODOS os atributos que são passados para a lista por meio da propriedade event */
 export const Card = ({event}) => {
+
+    const navigate = useNavigate();
 
     const formattedDate1 = new Intl.DateTimeFormat('pt-BR', {
         weekday: "short",
@@ -20,22 +23,22 @@ export const Card = ({event}) => {
     const differentDates = event.data_inicio !== event.data_fim;
 
     return(
-        <div className={styles.card} onClick={() => alert(`Clicou no card: ${event.nome}`)}>
+        <div className="card" onClick={() => navigate(`/event/${event.id}`, {state: event})}>
             <img src={event.imagem} className="card-img-top" alt={event.nome} />           
-            <div className={styles.cardHeader}>
+            <div className="cardHeader">
             
             <Calendar2Event /> 
             {differentDates ? 
                 formattedDate2.format(new Date(event.data_inicio)) : formattedDate1.format(new Date(event.data_inicio)) 
             }
-            { differentDates ? (<ChevronRight />) : ("") }
+            { differentDates && (<ChevronRight />) }
             { differentDates && formattedDate2.format(new Date(event.data_fim)) } 
                                      
             </div>
-            <div className={styles.cardBody}>
-                <h5 className={styles.cardTitle}> {event.nome} </h5>
-                <p className={styles.cardText}>
-                { <PinMap />} {event.local.nome} - {event.local.cidade}, {event.local.estado}
+            <div className="cardBody">
+                <h5 className="cardTitle"> {event.nome} </h5>
+                <p className="cardText">
+                 <PinMap /> {event.local.nome} - {event.local.cidade}, {event.local.estado}
                 </p>
             </div>
         </div>
