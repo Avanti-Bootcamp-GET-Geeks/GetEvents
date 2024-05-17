2984ec20aec555b70902be7c5a8c8cd9136d1764
Giovane Souza <developergiovanesouza@gmail.com>
2024-05-17 16:10:56 -0300

    refactor: adaptação do código para renderização conforme add params

--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -1,50 +1,23 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Card } from "../../../components/card/Card";
 import { EventList } from "../../../components/eventList/EventList";
-import { findAllEvents } from "../../../services/eventService";
 import { Pagination } from "../../../components/pagination/Pagination";
+import { SearchContext } from "../../../context/SearchContext";
 
 export default function Home() {
-
-    const [events, setEvents] = useState([]);
-    const [categoria, setCategoria] = useState("");
-    const [local, setLocal] = useState("");
-    const [data, setData] = useState("");
-    const [currentPage, setCurrentPage] = useState(1);
-
-    useEffect(()=> {
-        getAllEvents();
-    }, [currentPage])
-
-
-    const getAllEvents = async () => {
-        const limit = 10;
-        const offset = (currentPage - 1) * limit;
-
-        try {
-            const response = await findAllEvents(categoria, local, data, limit, offset);
-            if(response) {
-                setEvents(response);
-            } else setEvents([]);
-        } catch(error) {
-            console.log(error.message)
-        }
-    };
-
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-    };
-
+    const {events, eventsFound, currentPage, handlePageChange} = useContext(SearchContext);
+    
     return(
         <>
-
             <div className="container">
                 {/* Lista eventos */}
                 <EventList>
-                    { events.length > 0 ?
-                        (  events.map(event => (<Card event={event} key={event.id} />)  ) ) :
-
-                        <p style={{color: '#757679'}}>Nenhum evento encontrado.</p>
+                
+                {eventsFound.length > 0
+                        ? eventsFound.map(event => (<Card event={event} key={event.id} />))
+                        : events.length > 0
+                            ? events.map(event => (<Card event={event} key={event.id} />))
+                            : <p style={{ color: '#757679' }}>Nenhum evento encontrado.</p>
                     }
                 </EventList>
 
