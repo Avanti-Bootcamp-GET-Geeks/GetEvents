781e3747bf2b2b11292b09300e3b8a862d3b41a0
Iago Amorim <iagoamorim2010@gmail.com>
2024-05-17 15:34:36 -0300

    feat: Novas funções no register

--- src/pages/public/register/Register.jsx
+++ src/pages/public/register/Register.jsx
@@ -3,54 +3,71 @@ import { findAllRoles } from "../../../services/roleService"
 import { createUser } from "../../../services/userService"
 import { useNavigate } from "react-router-dom"
 
+import InputMask from "react-input-mask" // Módulo para criar input com mascaras
+
 export default function Register() {
+    // Informações dos usuários.
     const [nome, setName] = useState("")
     const [telefone, setPhone] = useState("")
     const [email, setEmail] = useState("")
     const [senha, setPassword] = useState("")
     const [cargo, setRole] = useState("")
 
-    const [cargos, setRoles] = useState([])
+    const [roles, setRoles] = useState([]) // Lista com os cargos do site.
+
+    const [errorEmail, setErrorEmail] = useState(false) // Boolean para verificar o email.
 
     const navigate = useNavigate()
 
-    async function getAllRoles() {
-        try {
-            const data = await findAllRoles()
-            setRoles(data)
-        } catch (error) {
-            console.error(error.message)
-        }
-    }
-
     useEffect(() => {
-        getAllRoles()
+        // Função para buscar a lista de cargos, execultado ao iniciar a página.
+        const fetchRoles = async () => {
+            try {
+                const data = await findAllRoles()
+                setRoles(data)
+            } catch (error) {
+                console.error(error.message)
+            }
+        }
+
+        fetchRoles()
     }, [])
 
-    const defineRole = (role) => {
+    // Função para lidar com a mudança de cargos, execultado ao clicar nos radios.
+    const handleRoleChange = (roleName) => {
         try {
-            const data = cargos.find((item) => item.nome.toLowerCase() == role.toLowerCase())
+            const data = roles.find((item) => item.nome.toLowerCase() == roleName.toLowerCase())
             if (data) {
-                setRole(data.id)
+                setRole(data.id) // Definir o cargo do usuário.
             }
         } catch (error) {
             console.error(error.message)
         }
     }
 
+    // Função de envio do formulário.
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const data = {
+        setErrorEmail(false)
+
+        const userData = {
             nome,
             email,
             telefone,
             senha,
             cargo_id: cargo,
         }
-        
-        await createUser(data)
 
-        navigate("/login")
+        try {
+            await createUser(userData)
+            navigate("/login")
+        } catch (error) {
+            if (error.message.includes("409")) {
+                setErrorEmail(true) // Ativa o erro no input do email.
+            } else {
+                console.error(error.message)
+            }
+        }
     }
 
     return (
@@ -67,30 +84,52 @@ export default function Register() {
                     <label htmlFor='inputEmail' className='form-label'>
                         Email
                     </label>
-                    <input type='email' className='form-control' id='inputEmail' placeholder='Digite email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    <input
+                        type='email'
+                        className={errorEmail ? "form-control is-invalid" : "form-control"}
+                        id='inputEmail'
+                        placeholder='Digite email'
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                            setErrorEmail(false)
+                        }}
+                        required
+                    />
+                    <div className='invalid-feedback'>Este email já está registrado.</div>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='inputTelefone' className='form-label'>
                         Telefone
                     </label>
-                    <input type='text' className='form-control' id='inputTelefone' placeholder='Digite seu telefone' value={telefone} onChange={(e) => setPhone(e.target.value)} required />
+                    <InputMask
+                        mask={telefone.length >= 6 && telefone[5] === "9" ? "(99) 99999-9999" : "(99) 9999-9999"}
+                        maskChar={""}
+                        type='text'
+                        className='form-control'
+                        id='inputTelefone'
+                        placeholder='Digite seu telefone'
+                        value={telefone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='inputSenha' className='form-label'>
                         Senha
                     </label>
-                    <input type='password' className='form-control' id='inputSenha' placeholder='Digite sua senha' value={senha} onChange={(e) => setPassword(e.target.value)} required />
+                    <input type='password' minLength='7' className='form-control' id='inputSenha' placeholder='Digite sua senha' value={senha} onChange={(e) => setPassword(e.target.value)} required />
                 </div>
 
                 <div className='mb-3 form-check'>
-                    <input className='form-check-input' type='radio' name='role' id='organizador' onChange={() => defineRole("organizador")} />
+                    <input className='form-check-input' type='radio' name='role' id='organizador' onChange={() => handleRoleChange("organizador")} />
                     <label className='form-check-label' htmlFor='organizador'>
                         Organizador
                     </label>
                 </div>
 
                 <div className=' mb-3 form-check'>
-                    <input className='form-check-input' type='radio' name='role' id='visitante' onChange={() => defineRole("visitante")} />
+                    <input className='form-check-input' type='radio' name='role' id='visitante' onChange={() => handleRoleChange("visitante")} />
                     <label className='form-check-label' htmlFor='visitante'>
                         Visitante
                     </label>
