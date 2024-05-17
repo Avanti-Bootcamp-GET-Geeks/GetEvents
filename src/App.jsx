29da5b789e0216ad24608abb61242562dc2ba960
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-05-17 01:31:48 -0300

    feat: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/public/home/Home'
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import './Global.css'
-import './App.css'
-import { EventInfo } from './pages/public/eventInfo/EventInfo'
-import Register from "./pages/public/register/Register.jsx";
-import Settings from "./pages/private/settings/Settings.jsx";
-import Login from "./pages/public/login/Login.jsx";
-import Categories from "./pages/private/categories/Categories.jsx";
-
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/public/home/Home';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import './Global.css';
+import './App.css';
+import {EventInfo} from './pages/public/eventInfo/EventInfo'
+import Register from './pages/public/register/Register';
+import Settings from './pages/private/settings/Settings';
+import Login from './pages/public/login/Login';
+import Categories from './pages/private/categories/Categories.jsx';
+import {SearchProvider} from './contexts/SearchContext.jsx';
+import {useState} from "react";
 
 export default function App() {
+    // Defina admin e setAdmin usando useState
+    const [admin, setAdmin] = useState(false);
+
     return (
-        <>
-            <Header/> {/* Renderiza o Header em todas as páginas */}
+        <SearchProvider>
+
+            <Header admin={admin} setAdmin={setAdmin}/>{/* Renderiza o Header em todas as páginas */}
             <main>
                 <Routes>
-                    <Route path='/' element={<Home/>}/> {/* Renderiza a Home diretamente */}
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/categories' element={<Categories />}/>
-                    <Route path='/event/:id' element={<EventInfo/>}/>
-                    <Route path='/register' element={<Register/>}/>
-                    <Route path='/settings' element={<Settings/>}/>
+                    <Route path="/" element={<Home/>}/> {/* Renderiza a Home diretamente */}
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/categories" element={<Categories/>}/>
+                    <Route path="/event/:id" element={<EventInfo/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
                 </Routes>
             </main>
             <Footer/>
-        </>
+
+        </SearchProvider>
     );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,101 +1,129 @@
-import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import './Header.css';
 import logo from '../../assets/img/logo.svg';
-import { findAllEvents } from "../../services/eventService.jsx";
+import { SearchContext } from '../../contexts/SearchContext.jsx';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import { BoxArrowRight, GearFill, Search } from "react-bootstrap-icons";
+import { Col } from "react-bootstrap";
+
+function removeAccents(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
 export default function Header() {
     const [search, setSearch] = useState("");
-    const [eventRender, setEventRender] = useState([]);
+    const { events, getAllEvents } = useContext(SearchContext);
+    const [event, setEvent] = useState("");
+    const [filteredEvents, setFilteredEvents] = useState([]);
     const [menuOpen, setMenuOpen] = useState(false);
-    const [settings, setSettings] = useState(false);
+    const [admin, setAdmin] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const filtered = eventRender.filter((event) =>
-            Object.values(event).some((value) =>
-                value.toString().toLowerCase().includes(search.toLowerCase())
-            )
+        const filtered = events.filter(event =>
+            removeAccents(event.nome.toLowerCase()).includes(removeAccents(search.toLowerCase()))
         );
-        if (filtered.length !== eventRender.length) {
-            setEventRender(filtered);
-        }
-    }, [eventRender, search]);
-
-
-    const handleRegister = () => {
-        navigate("/register");
-    };
+       console.log(filtered)
+        setFilteredEvents(filtered);
+    }, [events, search]);
 
     const handleBurgerClick = () => {
         setMenuOpen(!menuOpen);
     };
 
-    const handleSearch = () => {
-        findAllEvents()
-            .then((events) => {
-                setEventRender(events);
-            })
-            .catch((error) => {
-                console.error("Error fetching events:", error);
-            });
+    const handleRegister = () => {
+        navigate("/register");
+    };
+
+    const handleSearch = async () => {
+        console.log('teste')
+        await getAllEvents();
+    };
+
+    const handleLogin = () => {
+        setAdmin(true);
+        navigate("/login");
+    };
+
+    const handleLogout = () => {
+        setAdmin(false);
     };
 
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><img alt={'Logo Get Eventos'} src={logo}/></Link>
-                    <button className="navbar-toggler" type="button" onClick={handleBurgerClick}>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    {!settings ? (
-                        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarToggler">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" to="/">Crie seu Evento</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/login">Acesse sua conta</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <button className="btn btn-primary" onClick={handleRegister}>Cadastre-se
-                                    </button>
-                                </li>
-                            </ul>
-                            <form className="d-flex" role="search">
-                                <input className="form-control me-2 search-input" type="search"
-                                       placeholder="Pesquisar Eventos"
-                                       aria-label="Pesquisar" onChange={e => setSearch(e.target.value)}/>
-                                <button className="btn btn-outline-primary" type="button"
-                                        onClick={handleSearch}>Pesquisar
-                                </button>
-                            </form>
-                        </div>
-                    ) : (
-                        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarToggler">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" to="/">Crie seu Evento</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/settings">Bem vindo, ORGANIZADOR </Link>
-                                </li>
-                            </ul>
-                            <form className="d-flex" role="search">
-                                <input className="form-control me-2 search-input" type="search"
-                                       placeholder="Pesquisar Eventos"
-                                       aria-label="Pesquisar" onChange={e => setSearch(e.target.value)}/>
-                                <button className="btn btn-outline-primary" type="button"
-                                        onClick={handleSearch}>Pesquisar
-                                </button>
-                            </form>
-                        </div>
-
-                    )}
-                </div>
-            </nav>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand as={Link} to="/"><Image src={logo} fluid /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" onClick={handleBurgerClick} />
+                    <Navbar.Collapse id="navbarScroll" className={menuOpen ? 'show' : ''}>
+                        {admin ? (
+                            <>
+                                <Nav className="me-auto my-2 my-lg-0">
+                                    <Nav.Link as={Link} to="/">Crie seu Evento</Nav.Link>
+                                    <Nav.Link as={Link} to="/categories">Categorias</Nav.Link>
+                                    <Nav.Link as={Link} to="/settings" className={"link-underline-opacity-0-hover"}>
+                                        Bem Vindo, Organizador
+                                    </Nav.Link>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Pesquisar Eventos"
+                                        className="me-2"
+                                        aria-label="Search"
+                                        value={search}
+                                        onChange={(e) => setSearch(e.target.value)}
+                                    />
+                                    <Button variant="outline-primary"
+                                            type="button"
+                                            className='me-2'
+                                            onClick={handleSearch}>
+                                        <Search />
+                                    </Button>
+                                    <Button variant="secondary" className=' me-2' onClick={handleLogout}>
+                                        <BoxArrowRight />
+                                    </Button>
+                                    <Button as={Link} className={'text-black  '}
+                                            to="/settings" variant='warning'>
+                                        <GearFill />
+                                    </Button>
+                                </Form>
+                            </>
+                        ) : (
+                            <>
+                                <Nav className="me-auto my-2 my-lg-0">
+                                    <Nav.Link as={Link} to="/">Crie seu Evento</Nav.Link>
+                                    <Nav.Link onClick={handleLogin}>Login</Nav.Link>
+                                    <Col xs="auto" sm='auto'>
+                                        <Button variant="primary" onClick={handleRegister}>Cadastre-se</Button>
+                                    </Col>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Pesquisar Eventos"
+                                        className="me-2 "
+                                        aria-label="Search"
+                                        value={search}
+                                        onChange={(e) => setSearch(e.target.value)}
+                                    />
+                                    <Button variant="outline-primary" type="button"
+                                            onClick={handleSearch}>
+                                        <Search />
+                                    </Button>
+                                </Form>
+                            </>
+                        )}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
-    )
+    );
 }
