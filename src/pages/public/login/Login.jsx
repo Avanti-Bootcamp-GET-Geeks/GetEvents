31786fe0e3906a30131bbbacb01938c3796ff6c2
Giovane Souza <developergiovanesouza@gmail.com>
2024-05-17 00:08:23 -0300

    refactor: reestruturação dos campos

--- src/pages/public/login/Login.jsx
+++ src/pages/public/login/Login.jsx
@@ -24,39 +24,48 @@ export default function Login() {
     };
 
   
-    // Altera o type do input password, permitindo a visualização/ocultação da senha
-    const handleMessageClose = () => {
-        const divContent = divMessage.current;
-        const message = divContent.innerHTML;
+//    Altera o type do input password, permitindo a visualização/ocultação da senha
+    // const handleMessageClose = () => {
+    //     const divContent = divMessage.current;
+    //     const message = divContent.innerHTML;
 
-        setTimeout(()=> {
-            divContent.style.display = 'none';
-        }, 2000)
+    //     setTimeout(()=> {
+    //         divContent.style.display = 'none';
+    //     }, 2000)
 
-        console.log(message)
-    };
+    //     console.log(message)
+    // };
 
         // Sempre que for fazer uma requisição, utilizar função assíncrona (async)
         const handleSubmit = async (event) => {
             event.preventDefault();
     
             // Chama a função de login (do context) e passa os valores do form
-             await loginUser(fieldValue);              
-            
+             await loginUser(fieldValue);                   
         }
 
 
     return(
         <div className="container m-5">
 
-        <div className="message"ref={divMessage}>{state.message} {state.status}  
-        <button onClick={handleMessageClose}>x</button>
-        </div>
+         <h1 className="titulo">Login</h1>   
+
+         {/* <div className="message"ref={divMessage}>{state.message} {state.status}  
+            <button onClick={handleMessageClose}>x</button>
+        </div>  */}
 
             <form onSubmit={handleSubmit}>
-                <input type="text" name="email" id="" value={fieldValue.email} placeholder="email" onChange={handleChange} /> <br/>
-                <input type="password" name="senha" id="" value={fieldValue.senha} placeholder="senha"    onChange={handleChange}/> <br/>
-                <button type="submit">Entrar</button>
+                <div className="mb-3">
+                     <label htmlFor="inputLogin" className="form-label">E-mail</label>
+                     <input type="email" className="form-control" name="email" value={fieldValue.email} id="inputLogin" onChange={handleChange} />
+                 </div>
+
+                 <div className="mb-3">
+                     <label htmlFor="inputSenha" className="form-label">Senha</label>
+                     <input type="password" className="form-control" name="senha" value={fieldValue.senha} id="inputSenha" onChange={handleChange} />
+                 </div>
+
+               <button type="submit" className="btn btn-primary">Entrar</button>
             </form>
         </div>
     );
