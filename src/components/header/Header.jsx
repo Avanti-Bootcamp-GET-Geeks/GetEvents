8452d88129d6f0750a39150f2e3c888fa62d3521
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-05-18 04:20:41 -0300

    atualização: campos de busca header. fix: ajuste no layout do login para mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import {useContext, useEffect, useState} from "react";
-import { useNavigate, Link } from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom";
 import './Header.css';
 import logo from '../../assets/img/logo.svg';
 
@@ -9,142 +9,155 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
-import { BoxArrowRight, CalendarEvent, GearFill, PinMap, Search, Tag } from "react-bootstrap-icons";
-import { Col } from "react-bootstrap";
+import {BoxArrowRight, GearFill, PinMap, Tag} from "react-bootstrap-icons";
+import {Col, Row} from "react-bootstrap";
 import {AuthContext} from "../../context/AuthContext.jsx";
-import { SearchContext } from "../../context/SearchContext.jsx";
+import {SearchContext} from "../../context/SearchContext.jsx";
+
 
 export default function Header() {
 
-    const {categories, setCategory, locals, setLocalId} = useContext(SearchContext);
-    const {userLogged, logoutUser} = useContext(AuthContext);
-    const [menuOpen, setMenuOpen] = useState(false);
-    
-    const navigate = useNavigate();
+  const {categories, setCategory, locals, setLocalId} = useContext(SearchContext);
+  const {userLogged, logoutUser} = useContext(AuthContext);
 
-    const [fieldValue, setFieldValue] = useState({
-        categoria_id: "",
-        local_id: ""
+
+  const navigate = useNavigate();
+
+  const [fieldValue, setFieldValue] = useState({
+    categoria_id: "",
+    local_id: ""
+  });
+
+  useEffect(() => {
+    setCategory(fieldValue.categoria_id);
+    setLocalId(fieldValue.local_id);
+  }, [fieldValue])
+
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
+  const handleChange = (event) => {
+    setFieldValue({
+      ...fieldValue,
+      [event.target.name]: event.target.value
     });
-    
-    useEffect(()=> {
-        setCategory(fieldValue.categoria_id);
-        setLocalId(fieldValue.local_id);
-    }, [fieldValue])
-    
-
-    const handleRegister = () => {
-        navigate("/register");
-    };
-
-    const handleBurgerClick = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    const handleChange = (event) => {
-        setFieldValue({
-        ...fieldValue, 
-        [event.target.name]: event.target.value 
-    });
-    };
+  };
 
 
-    return (
-        <header>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container fluid>
-                    <Navbar.Brand as={Link} to="/"><Image src={logo} fluid /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" onClick={handleBurgerClick} />
-                    <Navbar.Collapse id="navbarScroll" className={menuOpen ? 'show' : ''}>
-                        {userLogged ? (
-                            <>
-                                <Nav className="me-auto my-2 my-lg-0">
-                                    <Nav.Link as={Link} to="/create/event">Crie seu Evento</Nav.Link>
-                                    <Nav.Link as={Link} to="/categories">Categorias</Nav.Link>
-                                    <Nav.Link as={Link} to="/settings" className={"link-underline-opacity-0-hover"}>
-                                        Bem Vindo, Organizador
-                                    </Nav.Link>
-                                </Nav>
-                                <Form className="d-flex">
-                                <>
-                                    {/* <div className="mb-3"> 
-                                    <label htmlFor="categoria" className="form-label"><Tag /> Categoria</label> <br/>
-                                        <select name="categoria_id" id="categoria" value={fieldValue.categoria_id} onChange={handleChange} className="form-select" aria-label="Default select example">
-                                            <option value="" disabled selected>Selecione uma categoria</option>
-                                            <option value="">Todas</option>
-                                                {categories.map(category => (
-                                                    <option value={category.id} key={category.id}>{category.nome}</option>
-                                                ))}
-                                        </select>
-                                    </div>
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/"><Image src={logo} fluid/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
 
-                                    <div className="mb-3 flex">                    
-                                            <label htmlFor="local" className="form-label"><PinMap /> Local</label> <br/>
-                                                <select name="local_id" id="local" value={fieldValue.local_id} onChange={handleChange}
-                                        className="form-select" aria-label="Default select example">
-                                            <option value="" disabled selected>Selecione um local</option>
-                                            <option value="">Todos</option>
+          {userLogged ? (
 
-                                            {locals.map(local => (
-                                            <option value={local.id} key={local.id}>{local.nome}</option>
-                                            ))}
-                                        </select>
-                                    </div> */}
-                                </>
+            <>
+              <Row>
+                <Col className={'mt-2 mb-2'}>
+                  <Form.Label><Tag/> Categorias</Form.Label>
+                  <Form.Select className='flex' name="categoria_id" id="categoria" value={fieldValue.categoria_id}
+                               onChange={handleChange} aria-label="Categorias">
+                    <option>Selecione uma categoria</option>
+                    <option value="">Todas</option>
+                    {categories.map(category => (
+                      <option value={category.id} key={category.id}>{category.nome}</option>
+                    ))}
+                  </Form.Select>
+                </Col>
+                <Col className={'mt-2 mb-2'}>
+                  <Form.Label><PinMap/> Local</Form.Label>
+                  <Form.Select
+                    name="local_id"
+                    id="local"
+                    value={fieldValue.local_id}
+                    onChange={handleChange}
+                    aria-label="Local"
+                  >
+                    <option>Selecione uma categoria</option>
+                    <option value="">Todos</option>
 
-                                    <Button variant="secondary" className=' me-2'
+                    {locals.map(local => (
+                      <option value={local.id} key={local.id}>{local.nome}</option>
+                    ))}
+                  </Form.Select>
 
-                                             onClick={() => logoutUser()} >
-                                        <BoxArrowRight />
-                                    </Button>
+                </Col>
+              </Row>
 
-                                    <Button as={Link} className={'text-black  '}
-                                            to="/settings" variant='warning'>
-                                        <GearFill />
-                                    </Button>
+              <Nav className="mt-2 ">
+                <Col>
+                  <Nav.Link as={Link} to="/settings">Bem Vindo, Organizador</Nav.Link>
+                </Col>
+                <Nav.Link as={Link} to="/create/event" className={'align-items-center'}>Crie seu Evento</Nav.Link>
+                <Nav.Link as={Link} to="/categories">Categorias</Nav.Link>
+              </Nav>
 
-                                </Form>
-                            </>
-                        ) : (
-                            <>
-                                <Nav className="me-auto my-2 my-lg-0">
-                                    <Nav.Link as={Link} to="/create/event">Crie seu Evento</Nav.Link>
-                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                                    <Col xs="auto" sm='auto'>
-                                        <Button variant="primary" onClick={handleRegister}>Cadastre-se</Button>
-                                    </Col>
-                                </Nav>
-                                <>
-                                    <div className="mb-3"> 
-                                    <label htmlFor="categoria" className="form-label"><Tag /> Categoria</label> <br/>
-                                        <select name="categoria_id" id="categoria" value={fieldValue.categoria_id} onChange={handleChange} className="form-select" aria-label="Default select example">
-                                            <option value="" disabled selected>Selecione uma categoria</option>
-                                            <option value="">Todas</option>
-                                                {categories.map(category => (
-                                                    <option value={category.id} key={category.id}>{category.nome}</option>
-                                                ))}
-                                        </select>
-                                    </div>
+              <Form className="d-flex mb-2 btn-icons">
+                <Button as={Link}
+                        to="/settings"
+                        className={' me-2'}
+                        variant='warning'>
+                  <GearFill/>
+                </Button>
+                <Button variant="secondary"
+                        onClick={() => logoutUser()}>
+                  <BoxArrowRight/>
+                </Button>
+              </Form>
 
-                                    <div className="mb-3 flex">                    
-                                            <label htmlFor="local" className="form-label"><PinMap /> Local</label> <br/>
-                                                <select name="local_id" id="local" value={fieldValue.local_id} onChange={handleChange}
-                                        className="form-select" aria-label="Default select example">
-                                            <option value="" disabled selected>Selecione um local</option>
-                                            <option value="">Todos</option>
+            </>
 
-                                            {locals.map(local => (
-                                            <option value={local.id} key={local.id}>{local.nome}</option>
-                                            ))}
-                                        </select>
-                                    </div>
-                                </>
-                            </>
-                        )}
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+          ) : (
+            <>
+              <Row>
+                <Col className={'mt-2 mb-2'}>
+                  <Form.Label><Tag/> Categorias</Form.Label>
+                  <Form.Select className='flex' name="categoria_id" id="categoria" value={fieldValue.categoria_id}
+                               onChange={handleChange} aria-label="Categorias">
+                    <option>Selecione uma categoria</option>
+                    <option value="">Todas</option>
+                    {categories.map(category => (
+                      <option value={category.id} key={category.id}>{category.nome}</option>
+                    ))}
+                  </Form.Select>
+                </Col>
+                <Col className={'mt-2 mb-2'}>
+                  <Form.Label><PinMap/> Local</Form.Label>
+                  <Form.Select
+                    name="local_id"
+                    id="local"
+                    value={fieldValue.local_id}
+                    onChange={handleChange}
+                    aria-label="Local"
+                  >
+                    <option>Selecione uma categoria</option>
+                    <option value="">Todos</option>
 
+                    {locals.map(local => (
+                      <option value={local.id} key={local.id}>{local.nome}</option>
+                    ))}
+                  </Form.Select>
+
+                </Col>
+              </Row>
+              <Col></Col>
+
+              <Nav className="">
+                <Nav.Link as={Link} to="/create/event">Crie seu Evento</Nav.Link>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Col xs="auto" sm='auto'>
+                  <Button variant="primary" onClick={handleRegister}>Cadastre-se</Button>
+                </Col>
+              </Nav>
+            </>
+          )}
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
