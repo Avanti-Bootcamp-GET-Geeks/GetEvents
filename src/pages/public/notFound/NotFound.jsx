35ac60770fbf4c09049a56edbba10fd46f21e28d
Luis Honorio <116170242+luishonoriocarvalho@users.noreply.github.com>
2024-05-19 16:26:42 -0300

    NotFound
    página de erro 404

---
+++ src/pages/public/notFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+    return (
+        <>
+            <div>
+                <h1>(^_^)</h1>
+                <p className="error">404 ERROR</p>
+                <p>Página não encontrada</p>
+            </div>
+        </>
+    )
+}
