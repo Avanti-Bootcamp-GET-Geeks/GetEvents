4a00d4600911d80bba84a8ecd00fe679eddfbad1
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-05-20 16:49:16 -0300

    fix redirect

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,18 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/public/home/Home';
-import {EventInfo} from '../pages/public/eventInfo/EventInfo';
+import { EventInfo } from '../pages/public/eventInfo/EventInfo';
 import Register from '../pages/public/register/Register';
 import Login from '../pages/public/login/Login';
 import NotFound from "../pages/public/notFound/NotFound.jsx";
 
 const PublicRoutes = () => (
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/event-info/:id' element={<EventInfo />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
-    </Routes>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/event-info/:id' element={<EventInfo />} />
+    <Route path='/register' element={<Register />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/404' element={<NotFound />} />
+    <Route path='*' element={<Navigate to="/404" replace />} />
+  </Routes>
 );
 
 export default PublicRoutes;
